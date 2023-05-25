660d2acd87aa30e2845e9b9605ca1c6f98e67c9b
kefak <61420378+kefaczur@users.noreply.github.com>
2023-05-25 22:08:19 +0200

    new django app
    nowa aplikacja + strona startowa

--- PD_Projekt/settings.py
+++ PD_Projekt/settings.py
@@ -33,12 +33,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gameshop.apps.GameshopConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
