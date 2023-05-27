5541e745443912ee2d756ed8a1e241bac483f4da
Adam Rzepka <adamrzepka32167@gmail.com>
2023-05-27 15:49:10 +0200

    product.html temp
    temporary view for product site.

--- PD_Projekt/urls.py
+++ PD_Projekt/urls.py
@@ -19,8 +19,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('gameshop/', include('gameshop.urls')),
     path('admin/', admin.site.urls),
+    path('', include('gameshop.urls',namespace='gameshop')),
 ]
 
 if settings.DEBUG:
--- gameshop/views.py
+++ gameshop/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "gameshop/index.html")
+
+def products(request):
+    return render(request, 'gameshop/products.html')
+
+
